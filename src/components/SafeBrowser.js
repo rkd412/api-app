40b3cb737cf008554efd9a75dfa2165ebeaaa666
Robert Davis <rkdavis15@gmail.com>
2021-10-13 15:49:42 -0400

    Learning about AJAX

--- src/components/SafeBrowser.js
+++ src/components/SafeBrowser.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const SafeBrowser = () => {
-  return <h1>Safe Browser!!</h1>;
+  return (
+    <>
+      <h1>Safe Browser!!</h1>
+      <form method="POST">
+        <input type="text" />
+        <input type="submit" />
+      </form>
+    </>
+  );
 };
 
 export default SafeBrowser;
